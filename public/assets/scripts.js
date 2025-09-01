a3c3b4f3b70b5b198f7b6fd4e09ee5e01b3cb3f9
Michael Ahern <michael.ahern@gmail.com>
2025-08-31 19:48:49 -0600

    IDE Code Formatting (#10)

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,27 +1,27 @@
-document.getElementById('white').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'white';
+document.getElementById('white').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'white';
 };
 
-document.getElementById('blue').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'blue';
+document.getElementById('blue').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'blue';
 };
 
-document.getElementById('green').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'green';
+document.getElementById('green').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'green';
 };
 
-document.getElementById('amber').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'amber';
+document.getElementById('amber').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'amber';
 };
 
-document.getElementById('rgb-red').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'rgb-red';
+document.getElementById('rgb-red').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'rgb-red';
 };
 
-document.getElementById('rgb-green').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'rgb-green';
+document.getElementById('rgb-green').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'rgb-green';
 };
 
-document.getElementById('rgb-blue').onclick = function() {
-    document.getElementsByTagName('pre')[0].className = 'rgb-blue';
+document.getElementById('rgb-blue').onclick = function () {
+  document.getElementsByTagName('pre')[0].className = 'rgb-blue';
 };
